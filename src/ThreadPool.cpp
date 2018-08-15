95380f0b497566ea023e814e8a20e45bd8af16da
varunsharma0286 <vsharma7@buffalo.edu>
2018-08-14 23:02:26 -0400

    Changes

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -3,14 +3,38 @@
 //Default Constructor
 ThreadPool::ThreadPool()
 {
-	m_nNumThreads = 1;	
+	m_nNumThreads = 1;
+	m_bIsInitialized = false;
 }
 
 ThreadPool::ThreadPool(unsigned nNumThreads)
 {
+	m_bIsInitialized = false;
 	if(nNumThreads == 0)
 		m_nNumThreads = 1;
 	else
-		nNumThreads = nNumThreads;
+		m_nNumThreads = nNumThreads;
 }
 
+//Note:: This function "MUST" be called from the main thread
+bool ThreadPool::Init()
+{
+	if(m_bIsInitialized)
+		return true;
+
+		
+}
+
+void ThreadPool::AssignTask(string sLog)
+{
+	if(m_nNumThreads == 1)
+	{
+		m_ThreadPool[0].assign(sLog);
+		return;
+	}
+
+	
+	//When we come here, the value of m_nThreadIndex is guaranteed to be between 0 and m_nNumThreads-1
+	unsigned nThreadIndex = m_nThreadIndex++; 
+	nThreadIndex = nThreadIndex%m_nNumThreads;
+}
