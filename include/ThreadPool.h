60fdc694c0f2fd4127cd248114b56bfcda75e53c
varunsharma0286 <vsharma7@buffalo.edu>
2018-08-14 23:02:16 -0400

    Changes

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -3,12 +3,36 @@
  *using C++11
  *To use this ThreadPool, create
  *1) Create a ThreadPool object
- *2) Call the Init;
+ *2) Call the Init from the main thread;
  *3) Assign a task to ThreadPool using AssignTask call
  */
 
+#include<thread>
+#include<iostream>
+
+
+
 #ifndef _THREADPOOL_H
 #define _THREADPOOL_H
+
+//This Thread will be used by "ThreadPool"
+struct Thread
+{
+	std::thread mThread;
+	std::queue<string> mQueue;
+	void Assign(string sLog)
+	{
+		std::unique_lock<std::mutex> lock(m_Mutex);
+		mQueue.push_back(sLog);
+	}
+	
+	void Run()
+	{
+		
+	}
+
+	std::mutex m_Mutex;
+}
 
 class ThreadPool
 {
@@ -17,9 +41,16 @@ public:
 
 	ThreadPool(unsigned nNumThreads);
 
+	bool Init();
+
+	void AssignTask();
+	//void ResizeThreadPool(unsigned nNumThreads)
+	
 private:
 	unsigned m_nNumThreads;
-	vector<std::thread> ThreadPool;
+	vector<Thread> m_ThreadPool; 
+	bool m_bIsInitialized;
+	atomic<unsigned> m_nThreadIndex = 0; //Initialized to point to first thread;
 };
 
 #endif
