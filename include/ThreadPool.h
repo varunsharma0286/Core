c9341d000401cbcdbf74ed6773a3acf41048cbf1
varunsharma0286 <vsharma7@buffalo.edu>
2018-08-15 23:26:31 -0400

    Adding code with codition variables

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -8,6 +8,8 @@
  */
 
 #include<thread>
+#include <mutex>
+#include <condition_variable>
 #include<iostream>
 
 
@@ -18,20 +20,84 @@
 //This Thread will be used by "ThreadPool"
 struct Thread
 {
-	std::thread mThread;
-	std::queue<string> mQueue;
+public:
+	Thread() = delete;
+	Thread(const Thread&) = delete;
+	void operator = (const Thread&) = delete;
+
+	Thread(unsigned nId):
+	m_Thread(),
+	m_nThradId(nId),
+	m_bStopThread(false)
+	{
+	}
+	
+	std::thread m_Thread;
+	std::queue<string> m_Queue;
 	void Assign(string sLog)
 	{
 		std::unique_lock<std::mutex> lock(m_Mutex);
 		mQueue.push_back(sLog);
-	}
-	
-	void Run()
-	{
-		
+		m_ConditionVar.notify_one();
 	}
 
+	void StartThread()
+	{
+		m_Thread = std::thread(&Thread::ThreadFunction,this);
+	}
+
+	void StopThread()
+	{
+		std::unique_lock<std::mutex> lock(m_Mutex);
+		m_bStopThread = true;
+		m_ConditionVar.notify_one();
+	}
+
+	void ThreadFunction();
+	{
+		while(not m_bStopThread)
+		{
+			std::unique_lock<std::mutex> lock(m_Mutex);
+			while(not m_Queue.empty())
+			{
+				std::cout<<m_nThreadId<<":"<<m_Queue.front()<<std::endl;
+				m_Queue.pop();
+			}
+			m_ConditionVar.wait(lock);
+			
+			//This wait takes a predicate that is used to loop until it returns false; this overload may be used to avoid spurious wakeups.
+			/*m_ConditionVar.wait(lock, [&](){return !m_Queue.empty();});
+			while(not m_Queue.empty())
+			{
+				std::cout<<m_nThreadId<<":"<<m_Queue.front()<<std::endl;
+				m_Queue.pop();
+			}*/
+		}
+
+		//Finish up the leftover	
+		std::unique_lock<std::mutex> lock(m_Mutex);
+		while(not m_Queue.empty())
+		{
+			std::cout<<m_nThreadId<<":"<<m_Queue.front()<<std::endl;
+			m_Queue.pop();
+		}
+	
+		std::cout<<"Exiting thread:"<<m_nThreadId<<std::endl;
+	}
+
+	~Thread()
+	{
+		if(not m_bStopThread)
+		{
+			StopThread();
+		}
+	}
+
+private:
 	std::mutex m_Mutex;
+	std::condition_variable m_ConditionVar;
+	const unsigned m_nThreadId;
+	bool m_bStopThread;
 }
 
 class ThreadPool
--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -22,11 +22,31 @@ bool ThreadPool::Init()
 	if(m_bIsInitialized)
 		return true;
 
-		
+	for(unsigned i=0;i<m_nNumThreads;i++)
+	{
+		m_ThreadPool.emplace_back(Thread(i));
+		if(not m_ThreadPool[i].StartThread())
+		{
+			//Failed to initialize this thread... stop all the threads and exit;
+			for(unsigned j=0;j<i;j++)
+			{
+				m_ThreadPool[j].StopThread();
+			}
+			return false;
+		}
+	}
+
+	std::cout<<"ThreadPool initialized successfully"<<std::endl;
+	return true;
 }
 
 void ThreadPool::AssignTask(string sLog)
 {
+	if(not m_bIsInitialized)
+	{
+		return;
+	}
+
 	if(m_nNumThreads == 1)
 	{
 		m_ThreadPool[0].assign(sLog);
@@ -37,4 +57,5 @@ void ThreadPool::AssignTask(string sLog)
 	//When we come here, the value of m_nThreadIndex is guaranteed to be between 0 and m_nNumThreads-1
 	unsigned nThreadIndex = m_nThreadIndex++; 
 	nThreadIndex = nThreadIndex%m_nNumThreads;
+	m_ThreadPool.Assign(sLog);
 }
